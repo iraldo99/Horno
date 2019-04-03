e83e998fb990bad1381ffb0e25b5902f587cb372
infor22@aico <infor22@aico>
2019-04-03 19:04:43 +0200

    Creacion del proyecto

---
+++ src/main/java/lasencinas/org/Horno/App.java
@@ -0,0 +1,13 @@
+package lasencinas.org.Horno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
