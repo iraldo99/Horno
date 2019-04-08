daf207f6e208145c8ecfecc3aae07bb4f5487037
iraldo99 <igonzalezc@iesfbmoll.org>
2019-04-09 01:13:27 +0200

    Implementada clase TemperaturaAmbiente utlizando el patron Singleton

--- src/main/java/lasencinas/org/Tipos/TemperaturaAmbiente.java
+++ src/main/java/lasencinas/org/Tipos/TemperaturaAmbiente.java
@@ -2,10 +2,31 @@ package lasencinas.org.Tipos;
 
 public class TemperaturaAmbiente {
 	private double temp = 0;
+	private static final TemperaturaAmbiente INSTANCE = new TemperaturaAmbiente();
 	
-	public TemperaturaAmbiente() {
+	private TemperaturaAmbiente() {
 	}
 	
+	public static TemperaturaAmbiente getInstance() {
+		return INSTANCE;
+	}
 	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+	
+	public double getTemp() {
+		return temp;
+	}
+	
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+	
+	public void subirTemperatura(double incremento) {
+		this.temp += incremento;
+	}
 	
 }
